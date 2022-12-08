081703fc08efcf5a385af9f26c5c9371c3c4a2e1
Mike Bostock <michael.bostock@opencastsoftware.com>
2022-12-08 14:05:37 +0000

    minor refactor

--- src/main/kotlin/mpbostock/Types.kt
+++ src/main/kotlin/mpbostock/Types.kt
@@ -3,9 +3,7 @@ package mpbostock
 import kotlin.math.abs
 
 data class Vector2d(val x: Int, val y: Int) {
-    fun abs(other: Vector2d) = if ( this.x == other.x ) abs(this.y - other.y) else abs(
-        this.x - other.x
-    )
+    fun abs(other: Vector2d) = if ( this.x == other.x ) abs(this.y - other.y) else abs(this.x - other.x)
     companion object {
         fun fromFile(coord: String): Vector2d {
             val csv = coord.split(',').map { it.toInt() }
